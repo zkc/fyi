4b6eecc0263ace882b76ae85ebe853c363858a89
zkc <zkyle12@gmail.com>
2018-06-13 21:24:40 -0600

    delete and try fresh with styled components

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,68 +1,49 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
 
-// import Navigator from './Navigator'
-import Resume from './Resume'
-import Contact from './Contact'
-import Project from './Project'
-import '../Style/App.css';
+import Navigator from './Navigator'
+// import Resume from './Resume'
+// import Contact from './Contact'
+// import Project from './Project'
+
+const Site = styled.div`
+  display: grid;
+
+  position: relative;
+  grid-template-areas: 
+    "ls header rs"  
+    "ls quote rs"
+    "ls bio rs"
+    "ls projects rs"
+    "ls resume rs"
+    "ls contact rs"
+  ;
+  grid-template-columns: 1fr 87vw 1fr;
+  grid-auto-rows: max-content;
+
+  justify-items: center;
+`
+
 
 
 class App extends Component {
   constructor(props) {
     super(props)
 
-    this.display_modes = [
-      "designer_mode", // swap css to something more colorful, with animations!
-      "dev_mode", //show raw json ? drag and drop lol?
-      "good_website_mode", //just plain text, less spacing
-      "honesty_mode", /// tell the long truth. expand on bio
-      "game_mode", /// hide logos of web tech and give point for findng/clicking on them.
-    ]
-
 
     this.state = {
       mode: "in dev"
     }
-
-    // console.log(document.styleSheets)
-    // load in :root class, change sheet with state. could be own piece 
 
   }
 
   render() {
     const { basics, quote, bio, projects, resume, contact } = this.props
     return (
-      <div className="main-page" >
-        {/* <Navigator mode={this.state.mode} /> */}
-        <div className="ls" />
-        <div className="rs" />
-        
-        <header id="top" className="header"> 
-          <h1>{basics.name}</h1>
-          <h2>{basics.title}</h2>
-          <div className="bar" />
-        </header>
-
-        <section className="quote">
-          <h3>"{quote.words_first_half}</h3>
-          <h3>{quote.words_second_half}"</h3>          
-          <h4>- {quote.speaker}</h4>
-        </section>
-
-        <section className="bio">
-          <p>{bio.topper}</p>
-          <p>{bio.body}</p> 
-        </section>
-
-        <section className="projects">
-        {
-          projects.map(p => <Project {...p} key={p.title}/>)
-        }
-        </section>
-
-        <Resume {...resume} mode={this.state.mode} />
-        <Contact {...contact} />
-      </div>
+      <Site>
+        <Navigator />
+        Hello!
+      </Site>
     )
   }
 }
--- src/Components/Navigator.js
+++ src/Components/Navigator.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Navigation = styled.nav`
+  width: 600px;
+  height: 300px;
+  border: solid black 1px;
+`
 
 class Navigator extends Component {
   constructor(props) {
@@ -22,66 +29,9 @@ class Navigator extends Component {
 
 
     return (
-      <div className="navigator"
-        onTouchStart={(e) => {
-          console.log('touch start', e, this.state)
-          if (this.state.is_open) {
-            console.log('is open')
-            this.setState({clickOk: true})
-          } else {
-
-            e.stopPropagation()  
-          }
-          this.setState({is_open: true,  touch: true})
-        }}
-        onTouchEnd={(e) => {
-          console.log('touch end', e)
-          // this.setState({is_open: false, touch:false})
-        }}
-        onClick={(e) => {
-          if (this.state.touch) {
-            if (this.state.clickOk) {
-              console.log('stop the click')
-              this.setState({clickOk: false})
-            } else {
-              e.preventDefault()
-  
-            }
-          }
-          console.log('click', e)
-          // if it's just been touched, don't click the thing. Else, click the thing
-
-        }}
-        onMouseEnter={(e) => {
-          if (!this.state.touch) {
-            console.log('mouse enter', e)
-            this.setState({is_open: true})
-
-          } 
-
-        }} 
-        onMouseLeave={(e) => {
-          if (!this.state.touch) {
-            console.log('mouse leave', e)  
-          
-            this.setState({is_open: false})
-
-          }
-                  
-        }}
-        >
-        {this.state.is_open ? 
-          <ul className="full-menu">
-            <li><a href="#top">Top</a></li>
-            <li><a href="#resume">Resume</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li>...</li>
-          </ul> 
-          :
-          <div className="closed-menu" />
-        }
-        
-      </div>
+      <Navigation>
+        I get a round
+      </Navigation>
     )
   }
 }
